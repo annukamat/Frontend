c93942c2bbe6903e08cd41450a0de1d58d947afc
annukamat <annukamat21@gmail.com>
2020-11-04 18:11:27 +0530

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Redirect, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/home";
 import Navbar from "./components/Navbar/navbar";
 
-const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Redirect to="/" />
-			</Switch>
-		</>
-	);
-};
+class App extends Component {
+	render() {
+		return (
+			<>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Redirect to="/" />
+				</Switch>
+			</>
+		);
+	}
+}
 
 export default App;
